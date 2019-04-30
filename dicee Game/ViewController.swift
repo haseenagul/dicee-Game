6ec3483f52353e8635a938434299020853e87096
haseena gul <haseenagul100@gmail.com>
2019-04-30 15:41:34 +0500

    add rolling functionality ....

--- dicee Game/ViewController.swift
+++ dicee Game/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomindex1 : Int = 0
+    var randomindex2 : Int = 0
+    
+ let diceArray = ["dice1","dice2","dice3" ,"dice4" ,"dice5" ,"dice6"]
 
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+  
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rollButtonPressed()
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: Any) {
+    
+    
+        rollButtonPressed()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        rollButtonPressed()
+    }
+    
+    
+    
+    func rollButtonPressed (){
+        
+        // Create random indexs for imgaes..
+        randomindex1 = Int.random(in: 0...5)
+        randomindex2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomindex1])
+        diceImageView2.image = UIImage(named: diceArray[randomindex2])
+    }
+    
 }
 
